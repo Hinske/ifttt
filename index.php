de4939cbe4a7f878fa761fbb131659569c815e30
Hinske <markushelmis@googlemail.com>
2015-03-23 12:15:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 //Redirect to the github project for information about the webhook (my fork)
-header("Location: https://github.com/mapkyca/ifttt-webhook");
+header("Location: https://github.com/hinske/ifttt-webhook");
